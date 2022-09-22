f3da075504dd57464bc414537ed7bb2dacb92f1a
deondazy <deondazy@gmail.com>
2022-09-22 15:55:44 +0100

    Add Terms and Conditions page

---
+++ terms.php
@@ -0,0 +1,30 @@
+<?php
+require __DIR__ . '/bootstrap.php';
+
+$page = 'Terms';
+
+include OKOYE_ROOT . '/header.php';
+?>
+
+<section class="headings">
+    <div class="text-heading text-center">
+        <div class="container">
+            <h1><?php echo $page; ?></h1>
+        </div>
+    </div>
+</section>
+<!-- END SECTION HEADINGS -->
+
+<!-- START SECTION ABOUT -->
+<section class="about-us fh">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12 col-md-12 col-xs-12">
+                <p class="text-center">Placeholder Text</p>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- END SECTION ABOUT -->
+
+<?php include OKOYE_ROOT . '/footer.php';
