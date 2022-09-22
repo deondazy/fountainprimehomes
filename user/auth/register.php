69304d0fa9e0d2a3e3aa57116ad987b04b5cdf07
deondazy <deondazy@gmail.com>
2022-09-22 15:26:25 +0100

    Add autofocus to register form

--- user/auth/register.php
+++ user/auth/register.php
@@ -22,28 +22,39 @@ include OKOYE_ROOT . '/header.php';
         <!-- START SECTION LOGIN -->
         <div id="login">
             <div class="login">
-                <form>
+                <form method="post">
                     <div class="form-group">
-                        <label>Email</label>
+                        <label for="fname">First name</label>
+                        <input type="text" class="form-control" name="fname" id="fname" autofocus>
+                    </div>
+                    
+                    <div class="form-group">
+                        <label for="lname">Last name</label>
+                        <input type="text" class="form-control" name="lname" id="lname">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="email">Email</label>
                         <input type="email" class="form-control" name="email" id="email">
-                        <i class="icon_mail_alt"></i>
                     </div>
+
                     <div class="form-group">
-                        <label>Password</label>
-                        <input type="password" class="form-control" name="password" id="password" value="">
-                        <i class="icon_lock_alt"></i>
+                        <label for="password">Password</label>
+                        <input type="password" class="form-control" name="password" id="password">
                     </div>
+
                     <div class="fl-wrap filter-tags clearfix add_bottom_30">
                         <div class="checkboxes float-left">
                             <div class="filter-tags-wrap">
                                 <input id="check-b" type="checkbox" name="check">
-                                <label for="check-b">Remember me</label>
+                                <label for="check-b">I accept the <a target="_blank" href="<?php echo $site->url; ?>/terms/">Terms and Conditions</a></label>
                             </div>
                         </div>
-                        <div class="float-right mt-1"><a id="forgot" href="javascript:void(0);">Forgot Password?</a></div>
                     </div>
-                    <a href="login.html#0" class="btn_1 rounded full-width">Login to Find Houses</a>
-                    <div class="text-center add_top_10">New to Find Houses? <strong><a href="register.html">Sign up!</a></strong></div>
+
+                    <button type="submit" id="submit" class="btn_1 rounded full-width">Login</button>
+                    
+                    <div class="text-center add_top_10">Already have an account? <strong><a href="<?php echo $site->url; ?>/login/">Login!</a></strong></div>
                 </form>
             </div>
         </div>
