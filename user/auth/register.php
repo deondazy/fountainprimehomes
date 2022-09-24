42805929b0ea476f1bc4ca9e4a7d14cc2dab3d79
deondazy <deondazy@gmail.com>
2022-09-25 00:27:33 +0100

    Update registration

--- user/auth/register.php
+++ user/auth/register.php
@@ -3,6 +3,8 @@ if (!session_id()) {
     session_start();
 }
 
+use Okoye\Core\Util;
+
 require __DIR__ . '/../../bootstrap.php';
 
 $page = 'Register';
@@ -20,7 +22,7 @@ if ("POST" == $_SERVER["REQUEST_METHOD"]) {
         "first_name"    => $firstName,
         "last_name"     => $lastName,
         "register_date" => $timeNow,
-    ], true);
+    ], false);
 
     if ($register) {
         Util::redirect($site->url . '/success/');
@@ -29,7 +31,7 @@ if ("POST" == $_SERVER["REQUEST_METHOD"]) {
             Util::flash('error', $error, 'alert alert-danger alert-styled-left');
         }
 
-        Util::redirect($site->url . '/sign-up/');
+        Util::redirect($site->url . '/register/');
     }
 }
 
@@ -37,56 +39,56 @@ include OKOYE_ROOT . '/header.php';
 ?>
 
 <section class="headings">
-            <div class="text-heading text-center">
-                <div class="container">
-                    <h1><?php echo $page; ?></h1>
+    <div class="text-heading text-center">
+        <div class="container">
+            <h1><?php echo $page; ?></h1>
+        </div>
+    </div>
+</section>
+<!-- END SECTION HEADINGS -->
+
+<!-- START SECTION LOGIN -->
+<div id="login">
+    <div class="login">
+        <?php Util::flash('error'); ?>
+        <?php Util::flash('success'); ?>
+
+        <form method="post">
+            <div class="form-group">
+                <label for="fname">First name</label>
+                <input type="text" class="form-control" name="fname" id="fname" autofocus>
+            </div>
+            
+            <div class="form-group">
+                <label for="lname">Last name</label>
+                <input type="text" class="form-control" name="lname" id="lname">
+            </div>
+
+            <div class="form-group">
+                <label for="email">Email</label>
+                <input type="email" class="form-control" name="email" id="email">
+            </div>
+
+            <div class="form-group">
+                <label for="password">Password</label>
+                <input type="password" class="form-control" name="password" id="password">
+            </div>
+
+            <div class="fl-wrap filter-tags clearfix add_bottom_30">
+                <div class="checkboxes float-left">
+                    <div class="filter-tags-wrap">
+                        <input id="check-b" type="checkbox" name="check">
+                        <label for="check-b">I accept the <a target="_blank" href="<?php echo $site->url; ?>/terms/">Terms and Conditions</a></label>
+                    </div>
                 </div>
             </div>
-        </section>
-        <!-- END SECTION HEADINGS -->
 
-        <!-- START SECTION LOGIN -->
-        <div id="login">
-            <div class="login">
-                <?php Util::flash('error'); ?>
-                <?php Util::flash('success'); ?>
-                
-                <form method="post">
-                    <div class="form-group">
-                        <label for="fname">First name</label>
-                        <input type="text" class="form-control" name="fname" id="fname" autofocus>
-                    </div>
-                    
-                    <div class="form-group">
-                        <label for="lname">Last name</label>
-                        <input type="text" class="form-control" name="lname" id="lname">
-                    </div>
-
-                    <div class="form-group">
-                        <label for="email">Email</label>
-                        <input type="email" class="form-control" name="email" id="email">
-                    </div>
-
-                    <div class="form-group">
-                        <label for="password">Password</label>
-                        <input type="password" class="form-control" name="password" id="password">
-                    </div>
-
-                    <div class="fl-wrap filter-tags clearfix add_bottom_30">
-                        <div class="checkboxes float-left">
-                            <div class="filter-tags-wrap">
-                                <input id="check-b" type="checkbox" name="check">
-                                <label for="check-b">I accept the <a target="_blank" href="<?php echo $site->url; ?>/terms/">Terms and Conditions</a></label>
-                            </div>
-                        </div>
-                    </div>
-
-                    <button type="submit" id="submit" class="btn_1 rounded full-width">Login</button>
-                    
-                    <div class="text-center add_top_10">Already have an account? <strong><a href="<?php echo $site->url; ?>/login/">Login!</a></strong></div>
-                </form>
-            </div>
-        </div>
-        <!-- END SECTION LOGIN -->
+            <button type="submit" id="submit" class="btn_1 rounded full-width">Create account</button>
+            
+            <div class="text-center add_top_10">Already have an account? <strong><a href="<?php echo $site->url; ?>/login/">Login!</a></strong></div>
+        </form>
+    </div>
+</div>
+<!-- END SECTION LOGIN -->
 
 <?php include OKOYE_ROOT . '/footer.php';
