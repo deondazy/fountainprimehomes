b3815d292a55b701cf4b79fc768f93fb85e30064
deondazy <deondazy@gmail.com>
2022-09-24 23:38:32 +0100

    Add user logout

---
+++ user/auth/logout.php
@@ -0,0 +1,17 @@
+<?php 
+if (!session_id()) {
+    session_start();
+}
+
+use Okoye\Core\Util;
+
+require __DIR__ . '/../../bootstrap.php';
+
+if ($user->isLogged()) {
+    $hash = Util::escape($_COOKIE[$config->cookie->login['name']]);
+	
+    if ($user->logout($hash)) {
+	    Util::flash('logout', 'You have logged out', 'alert alert-success');
+    	Util::redirect($site->url . '/login/');
+    }
+}
