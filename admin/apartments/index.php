a1c29c67d02d479d608e425e81c9850412bdb25c
deondazy <deondazy@gmail.com>
2022-09-28 11:52:06 +0100

    Add Admin All Apartments Management Page

---
+++ admin/apartments/index.php
@@ -0,0 +1,18 @@
+<?php 
+
+require __DIR__ . '/../../bootstrap.php';
+
+$file = 'apartments/';
+$pageName = 'Apartments';
+
+include OKOYE_ROOT . '/admin/header.php'; ?>
+
+<div class="col-lg-8">
+    <div class="panel panel-body"></div>
+</div>
+<div class="col-lg-4">
+    <div class="panel panel-body"></div>
+</div>
+
+
+<?php include OKOYE_ROOT . '/admin/footer.php';
