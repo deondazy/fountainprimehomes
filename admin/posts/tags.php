1ea0979533b374460cfada6bb1b59797e44e53d3
deondazy <deondazy@gmail.com>
2022-09-28 11:39:24 +0100

    Add Posts manangement to admin

---
+++ admin/posts/tags.php
@@ -0,0 +1,18 @@
+<?php 
+
+require __DIR__ . '/../../bootstrap.php';
+
+$file = 'posts/';
+$pageName = 'Tags';
+
+include OKOYE_ROOT . '/admin/header.php'; ?>
+
+<div class="col-lg-8">
+    <div class="panel panel-body"></div>
+</div>
+<div class="col-lg-4">
+    <div class="panel panel-body"></div>
+</div>
+
+
+<?php include OKOYE_ROOT . '/admin/footer.php';
