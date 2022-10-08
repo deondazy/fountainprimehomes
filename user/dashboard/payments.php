6664998e5909be446de6fb709b73666d93fc1347
deondazy <deondazy@gmail.com>
2022-10-08 15:31:29 +0100

    Create User Dashboard Payment page

---
+++ user/dashboard/payments.php
@@ -0,0 +1,15 @@
+<?php 
+
+require __DIR__ . '/../../bootstrap.php';
+
+$file = 'payments/';
+$pageName = 'Payments';
+
+include OKOYE_ROOT . '/user/dashboard/header.php'; ?>
+
+<div class="col-lg-12">
+    <div class="panel panel-body"></div>
+</div>
+
+
+<?php include OKOYE_ROOT . '/user/dashboard/footer.php';
